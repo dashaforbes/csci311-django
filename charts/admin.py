c88039d7a403cd10985d462810a53eeb7baaaeef
Callum Hogsden <orbinas@gmail.com>
2014-04-25 16:37:49 +0000

    add new db with couple bits of data

--- charts/admin.py
+++ charts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from charts.models import Issues, Component, Version, NosyList, Commit
+from charts.models import Issue, Component, Version, NosyList, Commit
 
 # Register your models here.
-admin.site.register(Issues)
+admin.site.register(Issue)
 admin.site.register(Component)
 admin.site.register(Version)
 admin.site.register(NosyList)
